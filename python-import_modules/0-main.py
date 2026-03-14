3868695c776708ec4b6b28e4b5a7d0b5aaf0f2c2
maleyka2908 <13485@holbertonstudents.com>
2026-03-14 17:49:51 +0000

    Fix directory structure and add task 0

--- python-import_modules/0-main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    """Print all integers of a list, one per line using str.format()"""
-    for num in my_list:
-        if not isinstance(num, int):
-            raise TypeError("All elements must be integers")
-        print("{}".format(num))

a13bf2136b331082d9d04d0ef1e0559f57ac1986
maleyka2908 <13485@holbertonstudents.com>
2026-03-15 17:01:52 +0000

    Add README and square_matrix_simple function

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    """Matrisin hər bir elementinin kvadratını hesablayıb yeni matris qaytarır"""
+    """
+    Computes the square value of all integers of a matrix.
+    Returns a new matrix of the same size.
+    """
     if matrix is None:
         return None
 
-    # Yeni matris yaradırıq (List comprehension daxilində list comprehension)
     return [[x**2 for x in row] for row in matrix]
+

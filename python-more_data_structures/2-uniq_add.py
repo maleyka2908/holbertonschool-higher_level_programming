fac4b6aefd9fa9818900935303cb6063a6341cef
maleyka2908 <13485@holbertonstudents.com>
2026-03-15 17:12:26 +0000

    Add function that adds unique integers

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    """
+    Adds all unique integers in a list (only once for each integer).
+    """
+    # Siyahını 'set'ə çeviririk ki, dublikatlar silinsin
+    unique_numbers = set(my_list)
+    
+    # Unikal rəqəmlərin cəmini hesablayıb qaytarırıq
+    return sum(unique_numbers)

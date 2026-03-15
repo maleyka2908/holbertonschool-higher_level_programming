7d0299e9e5e3e7c24f2d5e35f60d9420187607ad
maleyka2908 <13485@holbertonstudents.com>
2026-03-15 17:09:40 +0000

    Add search_replace function

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    """
+    Replaces all occurrences of an element by another in a new list.
+    """
+    if my_list is None:
+        return None
+
+    # Yeni list yaradırıq: əgər element 'search'-ə bərabərdirsə 'replace' qoy,
+    # əks halda elementin özünü saxla.
+    return [replace if x == search else x for x in my_list]

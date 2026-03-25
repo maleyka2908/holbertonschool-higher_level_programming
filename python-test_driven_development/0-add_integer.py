edf2486c45cbfcc794ce7bae8e243e49a84d0d69
maleyka2908 <13485@holbertonstudents.com>
2026-03-25 14:34:02 +0000

    Use type() instead of isinstance() to handle bools and let Python handle NaN natively

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 """
-This module provides a function for integer addition.
+This module provides a function that adds two integers.
 """
 
 
 def add_integer(a, b=98):
     """
-    Adds two integers.
+    Adds 2 integers.
+    Args:
+        a: first integer
+        b: second integer, defaults to 98
+    Returns:
+        The addition of a and b.
+    Raises:
+        TypeError: If a or b are not integers or floats.
     """
-    if not isinstance(a, (int, float)):
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)

9e070ff5e0781133c2d7174ecce25c42c3aba196
maleyka2908 <13485@holbertonstudents.com>
2026-03-25 16:59:50 +0000

    Fix PEP8 whitespace issues in Pascal triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -22,14 +22,9 @@ def pascal_triangle(n):
 
     for i in range(1, n):
         prev_row = triangle[-1]
-        # Hər sətir 1 ilə başlayır
         row = [1]
-        
-        # Ortadakı elementləri hesabla
         for j in range(len(prev_row) - 1):
             row.append(prev_row[j] + prev_row[j + 1])
-        
-        # Hər sətir 1 ilə bitir
         row.append(1)
         triangle.append(row)
 

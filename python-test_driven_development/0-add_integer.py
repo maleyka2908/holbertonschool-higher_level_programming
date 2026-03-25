9512cec63879de91e9f8e43841ffecc74b9389c9
maleyka2908 <13485@holbertonstudents.com>
2026-03-25 13:21:05 +0000

    Initialize repository and add project files

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module provides a function that adds two integers.
+The function ensures types are valid and handles casting.
+It is part of the Holberton project.
+"""
+
+
+def add_integer(a, b=98):
+    """Adds two integers or floats (casted to integers).
+    Returns the sum as an integer.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)

8a61f36c24a281e5f3f7a77c83ea2dbe625e5444
maleyka2908 <13485@holbertonstudents.com>
2026-03-25 14:29:18 +0000

    Handle NaN and Infinity edge cases

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
 """
-This module provides a function that adds two integers.
-The function ensures types are valid and handles casting.
-It is part of the Holberton project.
+This module provides a function for integer addition.
 """
 
 
 def add_integer(a, b=98):
-    """Adds two integers or floats (casted to integers).
-    Returns the sum as an integer.
+    """
+    Adds two integers.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    # NaN, Infinity və ya Overflow yarada biləcək çox böyük float yoxlaması
-    try:
-        a = int(a)
-    except (ValueError, OverflowError):
-        raise TypeError("a must be an integer")
-
-    try:
-        b = int(b)
-    except (ValueError, OverflowError):
-        raise TypeError("b must be an integer")
-
-    return a + b
+    return int(a) + int(b)

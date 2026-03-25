d26bf24ba5a9071d130b82d2673ff393f614c605
maleyka2908 <13485@holbertonstudents.com>
2026-03-25 16:22:17 +0000

    Add Task 0: read_file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-"""
-This module contains a function that reads a text file and prints it.
-"""
+"""Function that reads a text file (UTF8) and prints it to stdout."""
 
 
 def read_file(filename=""):
-    """
-    Reads a text file (UTF8) and prints it to stdout.
-
-    Args:
-        filename (str): The name of the file to read.
-    """
-    with open(filename, encoding="utf-8") as f:
+    """Reads a UTF-8 text file and prints its content to stdout."""
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")

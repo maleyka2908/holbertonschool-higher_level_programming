8e999801adc8dca0a3d12fd2318d50fe22cdcd97
maleyka2908 <13485@holbertonstudents.com>
2026-03-25 14:49:12 +0000

    Fix matrix_divided: handle infinity and argument errors

--- python-test_driven_development/1-matrix_divided.py
+++ python-test_driven_development/1-matrix_divided.py
@@ -7,35 +7,24 @@ This module provides a function that divides all elements of a matrix.
 def matrix_divided(matrix, div):
     """
     Divides all elements of a matrix by a divisor.
-
-    Args:
-        matrix: A list of lists of integers or floats.
-        div: A number (integer or float) to divide the matrix by.
-
-    Returns:
-        A new matrix with the results rounded to 2 decimal places.
-
-    Raises:
-        TypeError: If matrix is not a list of lists of integers/floats.
-        TypeError: If rows of the matrix are not the same size.
-        TypeError: If div is not a number.
-        ZeroDivisionError: If div is zero.
     """
-    err_msg = "matrix must be a matrix (list of lists) of integers/floats"
+    msg = "matrix must be a matrix (list of lists) of integers/floats"
 
-    if not isinstance(matrix, list) or not matrix or not matrix[0]:
-        raise TypeError(err_msg)
+    if not isinstance(matrix, list) or not matrix:
+        raise TypeError(msg)
 
-    if not all(isinstance(row, list) for row in matrix):
-        raise TypeError(err_msg)
-
-    row_len = len(matrix[0])
     for row in matrix:
-        if len(row) != row_len:
-            raise TypeError("Each row of the matrix must have the same size")
+        if not isinstance(row, list):
+            raise TypeError(msg)
         for elem in row:
             if not isinstance(elem, (int, float)):
-                raise TypeError(err_msg)
+                raise TypeError(msg)
+
+    if len(matrix) > 0:
+        row_size = len(matrix[0])
+        for row in matrix:
+            if len(row) != row_size:
+                raise TypeError("Each row of the matrix must have the same size")
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")

99e4650315f099a1db867bade9d15bb6074c7e8e
maleyka2908 <13485@holbertonstudents.com>
2026-03-25 15:46:29 +0000

    Handle all inf/nan and argument edge cases

--- python-test_driven_development/1-matrix_divided.py
+++ python-test_driven_development/1-matrix_divided.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-This module provides a function that divides all elements of a matrix.
+Module for matrix_divided method.
 """
 
 
 def matrix_divided(matrix, div):
     """
-    Divides all elements of a matrix by div, rounded to 2 decimal places.
+    Divides all elements of a matrix by div.
     """
     msg = "matrix must be a matrix (list of lists) of integers/floats"
 
@@ -21,8 +21,9 @@ def matrix_divided(matrix, div):
                 raise TypeError(msg)
 
     row_size = len(matrix[0])
-    if not all(len(row) == row_size for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
+    for row in matrix:
+        if len(row) != row_size:
+            raise TypeError("Each row of the matrix must have the same size")
 
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")

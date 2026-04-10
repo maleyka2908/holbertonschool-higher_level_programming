667a73f29fa032ee54208610ccef561f03d38101
maleyka2908 <13584@holbertonstudents.com>
2026-04-10 20:31:28 +0000

    Final cleanup for integer_validator tests

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module defines a BaseGeometry class with validation methods.
+This module defines a BaseGeometry class.
 """
 
 
@@ -18,14 +18,6 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """
         Validates the value as a positive integer.
-
-        Args:
-            name (str): The name of the parameter.
-            value (int): The value to validate.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than or equal to 0.
         """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))

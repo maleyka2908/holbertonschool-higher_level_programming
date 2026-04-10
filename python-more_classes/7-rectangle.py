f3dbed866e76b68b36cd1cb480098de9ca4facaa
maleyka2908 <13584@holbertonstudents.com>
2026-04-10 20:12:14 +0000

    Add print_symbol class attribute to Rectangle

---
+++ python-more_classes/7-rectangle.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+"""
+This module defines a Rectangle class with a customizable print symbol.
+"""
+
+
+class Rectangle:
+    """
+    Represents a rectangle.
+
+    Attributes:
+        number_of_instances (int): Total number of active instances.
+        print_symbol (any): Symbol used for string representation.
+    """
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """Initializes a new Rectangle and increments instance counter."""
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """Retrieves the width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width with validation."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieves the height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height with validation."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns the rectangle area."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Returns the rectangle perimeter."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Returns string representation using the print_symbol."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        rect_str = ""
+        for i in range(self.__height):
+            # We use str(self.print_symbol) to handle cases where it's a list/etc
+            rect_str += (str(self.print_symbol) * self.__width)
+            if i < self.__height - 1:
+                rect_str += "\n"
+        return rect_str
+
+    def __repr__(self):
+        """Returns string representation for eval()."""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Decrements counter and prints message on deletion."""
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")

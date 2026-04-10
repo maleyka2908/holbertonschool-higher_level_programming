a0b88e4bcfcf507550c47870b1171375bb96dfc9
maleyka2908 <13584@holbertonstudents.com>
2026-04-10 20:42:29 +0000

    Fix documentation and pycodestyle for add_integer

--- python-inheritance/python-test_driven_development/0-add_integer.py
+++ python-inheritance/python-test_driven_development/0-add_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This module provides a function that adds two integers.
-It ensures that the inputs are either integers or floats before adding.
+This module provides a function for integer addition.
+It performs type checking to ensure inputs are numbers.
 """
 
 
@@ -10,14 +10,14 @@ def add_integer(a, b=98):
     Adds two integers or floats after casting them to integers.
 
     Args:
-        a: The first number, must be an integer or float.
-        b: The second number, must be an integer or float (default is 98).
+        a: The first number.
+        b: The second number (defaults to 98).
 
     Returns:
         The sum of a and b as an integer.
 
     Raises:
-        TypeError: If either a or b is not an integer or a float.
+        TypeError: If a or b is not an integer or a float.
     """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
